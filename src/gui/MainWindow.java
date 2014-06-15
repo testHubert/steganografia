b6396db83e8718f13c653604f628fef6ff7b4dc7
J4r3q <j4r3q1992@gmail.com>
2014-06-15 14:13:03 +0200

    Funkcja wtapiania

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 
 import javax.imageio.ImageIO;
@@ -26,12 +28,19 @@ import org.imgscalr.Scalr;
 
 /** @author Hubert */
 public class MainWindow {
+	
+	private static enum Kolor {
+		CZERWONY, ZIELONY, NIEBIESKI
+	}
 
 	private final static String tBLAD = "B³¹d";
 	private final static String tWEJSCIE = "Scie¿ka do obrazka";
 	private final static String tWYJSCIE = "Gdzie zapisaæ obrazek z wtopion¹ b¹dŸ wyzerowan¹ wiadomoœci¹";
 	private final static String tWIADOMOSC = "Scie¿ka do pliku z wiadomoœci¹ lub gdzie zapisaæ ekstrahowan¹ wiadomoœæ";
 	private final static String bSZUKAJ = "Szukaj...";
+	private final static String eBRAKPLIKU = "Brak pliku w: ";
+	private final static String eMALY = "Za ma³y obrazek.";
+	private final static String SUKCES = "Operacja zakoñczona pomyœlnie.";
 
 	private boolean tryb = true;
 	private JFrame frame;
@@ -80,7 +89,101 @@ public class MainWindow {
 	public MainWindow() {
 		initGUI();
 	}
-
+	
+	private static void wtapianie(Kolor kolor) {
+		try {
+			wczytajWejscie();
+		} catch (IOException e) {
+			return;
+		}
+		/* Wczytanie wiadomoœci */
+		byte[] wiadomosc = null;
+		try {
+			wiadomosc = Files
+					.readAllBytes(Paths.get(sciezkaWiadomosc.getText()));
+		} catch (NoSuchFileException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(panel,
+					eBRAKPLIKU + e.getLocalizedMessage(), tBLAD,
+					JOptionPane.ERROR_MESSAGE);
+			sciezkaWiadomosc.setText(eBRAKPLIKU + sciezkaWiadomosc.getText());
+			return;
+		} catch (IOException | OutOfMemoryError e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(panel, e.getLocalizedMessage(),
+					tBLAD, JOptionPane.ERROR_MESSAGE);
+			sciezkaWiadomosc.setText(e.getLocalizedMessage());
+			return;
+		}
+		final int width = imgWej.getWidth();
+		final int height = imgWej.getHeight();
+		final int length = wiadomosc.length;
+		if (width * height >= Integer.SIZE + length * 8) {
+			/* Zapis d³ugoœci wiadomoœci */
+			byte[] wiadomoscBit = new byte[Integer.SIZE + length * 8];
+			int b = 0, maska = 0x80000000;
+			for (int i = Integer.SIZE - 1; i >= 0; i--) {
+				wiadomoscBit[b++] = (byte) ((length & maska) >> i);
+				maska >>>= 1;
+			}
+			/* Bajty do bitów */
+			for (int i = 0; i < length; i++) {
+				maska = 0x80;
+				for (int j = 7; j >= 0; j--) {
+					wiadomoscBit[b++] = (byte) ((wiadomosc[i] & maska) >> j);
+					maska >>>= 1;
+				}
+			}
+			/* Wybór maski koloru */
+			final int shift;
+			switch (kolor) {
+			case CZERWONY:
+				maska = 0x00010000;
+				shift = 16;
+				break;
+			case ZIELONY:
+				maska = 0x00000100;
+				shift = 8;
+				break;
+			case NIEBIESKI:
+				maska = 0x00000001;
+				shift = 0;
+				break;
+			default:
+				throw new IllegalArgumentException("Niepoprawny kolor: "
+						+ kolor);
+			}
+			/* Wtapianie */
+			b = 0;
+			int rgb;
+			BufferedImage imgWyj = new BufferedImage(width, height,
+					BufferedImage.TYPE_INT_RGB);
+			for (int x = 0; x < width; x++)
+				for (int y = 0; y < height; y++) {
+					rgb = imgWej.getRGB(x, y);
+					if (b < wiadomoscBit.length)
+						if (((rgb & maska) >> shift) != wiadomoscBit[b++])
+							rgb ^= maska;
+					imgWyj.setRGB(x, y, rgb);
+				}
+			imgWej.flush();
+			try {
+				zapiszWyjscie(imgWyj);
+			} catch (IOException e) {
+				return;
+			}
+			JOptionPane.showMessageDialog(panel, SUKCES + "\nZapisano "
+					+ length + " bajtów wiadomoœci.", "Sukces",
+					JOptionPane.INFORMATION_MESSAGE);
+		} else
+			JOptionPane.showMessageDialog(panel, eMALY + "\nWymiary obrazka: "
+					+ width + " x " + height + " = " + (width * height)
+					+ " pikseli - " + Integer.SIZE + " na d³ugoœæ = "
+					+ (width * height / 8 - Integer.SIZE / 8)
+					+ " bajtów mo¿liwych do zapisania.\nD³ugoœæ wiadomoœci: "
+					+ length + " bajtów.", tBLAD, JOptionPane.ERROR_MESSAGE);
+	}
+	
 	private static void wczytajWejscie() throws IOException {
 		try {
 			imgWej = ImageIO.read(new File(sciezkaWejscie.getText()));
